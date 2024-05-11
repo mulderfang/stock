0e96b6e7feaacbc489d35539f79b2dcd83577f91
Mulder Fang <43980730+mulderfang@users.noreply.github.com>
2024-05-11 11:18:19 +0800

    參考twiwan stock專案並改為撈ALL資料
    大改特改

---
+++ excute.py
@@ -0,0 +1,17 @@
+import Taiwan_Stocks as TS
+
+# If you don't have the MySQL database, just simply set  <------ IMPORTANT MESSAGE
+# db_settings = None                                     <------ IMPORTANT MESSAGE
+# MySQL_flag = False                                     <------ IMPORTANT MESSAGE
+# Fetch_stock_statistics_flag = False                    <------ IMPORTANT MESSAGE
+
+
+db_settings = { "host": "127.0.0.1",
+                "port": 3306,
+                "user": "root",
+                "password": "19970730",
+                "db": "sql_stock",
+                "charset": "utf8" }
+
+stocks = TS.Taiwan_Stocks( db_settings = db_settings, Crawl_flag = True, MySQL_flag = True,
+                           Fetch_stock_statistics_flag = True, timesleep = 5)
